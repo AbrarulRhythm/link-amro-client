3e36a894fbee4b4bd3674ce780bd250b4ad1d59b
Abrarul Rhythm <abrarulrhythm19912006@gmail.com>
2026-08-13 19:59:39 +0600

    setup Home Laytout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import { Outlet } from 'react-router';
 
 const MainLayout = () => {
     return (
-        <div>
-            <div>This is Main Layout</div>
+        // Mian Wrapper
+        <div className="main-wrapper">
+            {/* Header */}
+            <header>This is Header</header>
+            {/* Header End */}
+
+            {/* ==================== Mian ==================== */}
+            <main className="site-main">
+                <Outlet></Outlet>
+            </main>
+            {/* ==================== Mian End ==================== */}
+
+            {/* Footer */}
+            <footer>This is Site Footer</footer>
+            {/* Footer End */}
         </div>
+        // Mian Wrapper End
     );
 };
 
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from 'react-router';
 import MainLayout from '../layout/MainLayout';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import Home from '../pages/Home/Home/Home';
 
 export const router = createBrowserRouter([
     {
         path: '/',
         Component: MainLayout,
         errorElement: <ErrorPage></ErrorPage>,
-        // children: [],
+        children: [
+            {
+                path: '/',
+                Component: Home,
+            },
+        ],
     },
 ]);
