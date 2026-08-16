5e3e62475392c9a4177fa46f276c4eb7d8873f73
Abrarul Rhythm <abrarulrhythm19912006@gmail.com>
2026-08-16 17:00:52 +0600

    added link box

--- src/pages/Links/MyLinks/MyLinks.jsx
+++ src/pages/Links/MyLinks/MyLinks.jsx
@@ -1,6 +1,6 @@
-const MyLinks = () => {
-    const text = 'Personal Portfolio HTML Template & React template is also';
+import LinkBox from '../../Shared/LinkBox/LinkBox';
 
+const MyLinks = () => {
     return (
         <section>
             <div className="container">
@@ -13,29 +13,7 @@ const MyLinks = () => {
                 {/* Link Box Wrapper */}
                 <div className="mt-12">
                     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                        <div className="pt-4.5 px-4.5 pb-10.5 bg-white border border-dark-03 rounded-md hover:shadow-sm hover:cursor-pointer">
-                            <h3 className="text-base font-medium text-dark-09 mb-4">
-                                {text.length > 50 ? `${text.slice(0, 50)}...` : text}
-                            </h3>
-
-                            {/* Links */}
-                            <div className="text-sm flex flex-col gap-2.5">
-                                <a
-                                    href="https://mountainclimbersstar.com/demo/html/agkn"
-                                    target="_blank"
-                                    className="hover:text-dark-09 duration-100 wrap-anywhere"
-                                >
-                                    https://mountainclimbersstar.com/demo/html/agkn
-                                </a>
-                                <a
-                                    href="https://translate.google.com/?sl=en&tl=bn&op=translate"
-                                    target="_blank"
-                                    className="hover:text-dark-09 duration-100 active:text-primary"
-                                >
-                                    https://translate.google.com/?sl=en&tl=bn&op=translate
-                                </a>
-                            </div>
-                        </div>
+                        <LinkBox></LinkBox>
                     </div>
                 </div>
             </div>
--- src/pages/Shared/LinkBox/LinkBox.jsx
+++ src/pages/Shared/LinkBox/LinkBox.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+const text = 'Personal Portfolio HTML Template & React template is also';
 
 const LinkBox = () => {
     return (
-        <div>
-            <h1>This is Link Box</h1>
+        <div className="pt-4.5 px-4.5 pb-10.5 bg-white border border-dark-03 rounded-md hover:shadow-sm hover:cursor-pointer">
+            <h3 className="text-base font-medium text-dark-09 mb-4">{text.length > 50 ? `${text.slice(0, 50)}...` : text}</h3>
+
+            {/* Links */}
+            <div className="text-sm flex flex-col gap-2.5">
+                <a
+                    href="https://mountainclimbersstar.com/demo/html/agkn"
+                    target="_blank"
+                    className="wrap-anywhere hover:text-dark-09 duration-100 active:text-primary"
+                >
+                    https://mountainclimbersstar.com/demo/html/agkn
+                </a>
+            </div>
         </div>
     );
 };
